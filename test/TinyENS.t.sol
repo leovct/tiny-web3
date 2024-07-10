e49d9a14b53d8a87e758ee6eb05fc4298b172a26
leovct <l009.vincent@gmail.com>
2024-07-10 20:26:38 +0200

    chore: `forge fmt`

--- test/TinyENS.t.sol
+++ test/TinyENS.t.sol
@@ -1,80 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.21;
 
-import '../src/TinyENS.sol';
-import '@forge-std/Test.sol';
+import "../src/TinyENS.sol";
+import "@forge-std/Test.sol";
 
 contract TinyENSTest is Test {
-  TinyENS private tinyENS;
+    TinyENS private tinyENS;
 
-  address private owner = makeAddr('owner');
-  address private alice = makeAddr('alice');
-  address private bob = makeAddr('bob');
+    address private owner = makeAddr("owner");
+    address private alice = makeAddr("alice");
+    address private bob = makeAddr("bob");
 
-  function setUp() public {
-    vm.startPrank(owner);
-    tinyENS = new TinyENS();
-    console2.log('TinyENS deployed');
-    vm.stopPrank();
-  }
+    function setUp() public {
+        vm.startPrank(owner);
+        tinyENS = new TinyENS();
+        console2.log("TinyENS deployed");
+        vm.stopPrank();
+    }
 
-  function testRegisterNewName() public {
-    vm.startPrank(alice);
-    tinyENS.register('alice.eth');
-    vm.stopPrank();
+    function test_RegisterNewName() public {
+        vm.startPrank(alice);
+        tinyENS.register("alice.eth");
+        vm.stopPrank();
 
-    assertEq(tinyENS.resolve('alice.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice.eth');
-    console2.log('Alice registed alice.eth');
-  }
+        assertEq(tinyENS.resolve("alice.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice.eth");
+        console2.log("Alice registed alice.eth");
+    }
 
-  function testRegisterAlreadyRegisteredName() public {
-    vm.startPrank(alice);
-    tinyENS.register('alice.eth');
-    assertEq(tinyENS.resolve('alice.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice.eth');
-    console2.log('Alice registed alice.eth');
+    function test_RegisterAlreadyRegisteredName() public {
+        vm.startPrank(alice);
+        tinyENS.register("alice.eth");
+        assertEq(tinyENS.resolve("alice.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice.eth");
+        console2.log("Alice registed alice.eth");
 
-    console2.log('Bob tries to register alice.eth');
-    vm.startPrank(bob);
-    vm.expectRevert(TinyENS.AlreadyRegistered.selector);
-    tinyENS.register('alice.eth');
-    assertEq(tinyENS.resolve('alice.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice.eth');
-    assertEq(tinyENS.reverse(bob), '');
-    vm.stopPrank();
-  }
+        console2.log("Bob tries to register alice.eth");
+        vm.startPrank(bob);
+        vm.expectRevert(TinyENS.AlreadyRegistered.selector);
+        tinyENS.register("alice.eth");
+        assertEq(tinyENS.resolve("alice.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice.eth");
+        assertEq(tinyENS.reverse(bob), "");
+        vm.stopPrank();
+    }
 
-  function testUpdateWithNewName() public {
-    vm.startPrank(alice);
-    tinyENS.register('alice.eth');
-    assertEq(tinyENS.resolve('alice.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice.eth');
-    console2.log('Alice registed alice.eth');
+    function test_UpdateWithNewName() public {
+        vm.startPrank(alice);
+        tinyENS.register("alice.eth");
+        assertEq(tinyENS.resolve("alice.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice.eth");
+        console2.log("Alice registed alice.eth");
 
-    tinyENS.register('alice22.eth');
-    assertEq(tinyENS.resolve('alice22.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice22.eth');
-    console2.log('Alice updated its name to alice22.eth');
-    vm.stopPrank();
-  }
+        tinyENS.register("alice22.eth");
+        assertEq(tinyENS.resolve("alice22.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice22.eth");
+        console2.log("Alice updated its name to alice22.eth");
+        vm.stopPrank();
+    }
 
-  function testUpdateWithAlreadyRegisteredName() public {
-    vm.startPrank(alice);
-    tinyENS.register('alice.eth');
-    console2.log('Alice registed alice.eth');
+    function test_UpdateWithAlreadyRegisteredName() public {
+        vm.startPrank(alice);
+        tinyENS.register("alice.eth");
+        console2.log("Alice registed alice.eth");
 
-    vm.startPrank(bob);
-    tinyENS.register('bob.eth');
-    console2.log('Bob registed bob.eth');
+        vm.startPrank(bob);
+        tinyENS.register("bob.eth");
+        console2.log("Bob registed bob.eth");
 
-    console2.log('Bob tries to update its name to alice.eth');
-    vm.expectRevert(TinyENS.AlreadyRegistered.selector);
-    tinyENS.update('alice.eth');
-    assertEq(tinyENS.resolve('alice.eth'), alice);
-    assertEq(tinyENS.reverse(alice), 'alice.eth');
-    assertEq(tinyENS.resolve('bob.eth'), bob);
-    assertEq(tinyENS.reverse(bob), 'bob.eth');
-    vm.stopPrank();
-  }
+        console2.log("Bob tries to update its name to alice.eth");
+        vm.expectRevert(TinyENS.AlreadyRegistered.selector);
+        tinyENS.update("alice.eth");
+        assertEq(tinyENS.resolve("alice.eth"), alice);
+        assertEq(tinyENS.reverse(alice), "alice.eth");
+        assertEq(tinyENS.resolve("bob.eth"), bob);
+        assertEq(tinyENS.reverse(bob), "bob.eth");
+        vm.stopPrank();
+    }
 }
